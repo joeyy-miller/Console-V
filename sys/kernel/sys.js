e8b11b3432ce7e18ad97d89e68aaa953400d7a82
Joey Miller <34293717+joeyy-miller@users.noreply.github.com>
2024-02-22 21:06:33 -0800

    Code Refractoring
    Refactored setTimeout functions in sys.js to be more human readable. Previously they were all on one line, now the contents of the setTimeout functions are broken up to be easily readable.

--- sys/kernel/sys.js
+++ sys/kernel/sys.js
@@ -12,33 +12,81 @@ class System {
 		this.pauseStatus = null; //Store reason why system is paused
 	}
 
+	// Initilize taskbar
 	init_tb() {
-		setTimeout(function() {document.getElementById("topBar").innerHTML = '<a id="ijd" class="float-left"><i>i</i>JD <small>' + sys.versionShort + '</small></a><a id="program-name"></a><a id="time" style="float:right;"></a>'; },100);
-		setTimeout(function() {document.getElementById("time").style.display = "inline";}, 125)
+		setTimeout(function() {
+			document.getElementById("topBar").innerHTML = '<a id="ijd" class="float-left"><i>i</i>JD <small>' + sys.versionShort + '</small></a><a id="program-name"></a><a id="time" style="float:right;"></a>'; 
+		},100);
+		setTimeout(function() {
+			document.getElementById("time").style.display = "inline";
+		}, 125)
+
 		document.getElementById("topBar").innerHTML = '<a id="time" href=""></a>';
 		document.getElementById("topBar").style.borderBottom = '1px solid #fff';
 		display.time();
 	}
+
+	// Initilize right bar
 	init_rb() {
-		setTimeout(function() {document.getElementById("rightBar").innerHTML = 'sys sts: ok<br>';}, 200);
-		setTimeout(function() {document.getElementById("rightBar").innerHTML = 'sys sts: ok<br>kernel: ok<br>';}, 300);
-		setTimeout(function() {document.getElementById("rightBar").innerHTML = 'sys sts: ok<br>kernel: ok<br>sudo: ok<br>';}, 350);
-		setTimeout(function() {document.getElementById("rightBar").innerHTML = '<span class="_j" id="right_title">System Overviewd</span><br><span id="option1">sys sts: <span class="optcolr">' + kernel_task() + '</span></span><br><span id="option2">kernel: <span class="optcolr">ok</span></span><br><span id="option3">sudo: <span class="optcolr">ok</span></span><br><span id="option4">disp: <span class="optcolr">' + display.status() + "</span></span><br><span id='option5'>enabl colors</span><br><span id='option6'>enabl debug mode</span><br><span id='option7'>enabl run num</span><br><span id='option8'>trgr kernel panic</span>"}, 450);
-		setTimeout(function() {document.getElementById("rightBar").innerHTML = '<span id="option1">toggle txt colors</span><br>';}, 200);
-		setTimeout(function() {document.getElementById("rightBar").innerHTML = '<span id="option1">toggle txt colors</span><br>enable debug mode<br>';}, 300);
-		setTimeout(function() {document.getElementById("rightBar").innerHTML = '<span id="option1">toggle txt colors</span><br>enable debug mode<br>enable run num<br>';}, 350);
-		setTimeout(function() {document.getElementById("rightBar").innerHTML = '<span id="option1">toggle txt colors</span><br>enable debug mode<br>enable run num<br>trgr kernel panic';}, 400);
-		setTimeout(function() {document.getElementById("rightBar").innerHTML = '<span id="option1">toggle txt colors</span><br>enable debug mode<br>enable run num<br>trgr kernel panic<br><span id="option5">sys sts: <span class="optcolr">ok</span></span>';}, 500);
-		setTimeout(function() {document.getElementById("rightBar").innerHTML = '<span id="option1">toggle txt colors</span><br>enable debug mode<br>enable run num<br>trgr kernel panic<br><span id="option5">sys sts: <span class="optcolr">ok</span></span><br>kernel: <span class="optcolr">ok</span>';}, 560);
-		setTimeout(function() {document.getElementById("rightBar").innerHTML = "<span id='option1'>toggle txt colors</span><br><span id='option2'>enable debug mode</span><br><span id='option3'>enable run num</span><br><span id='option4'>trgr kernel panic</span><br><span id='option5'>sys sts: <span class='optcolr'>" + kernel_task() + '</span></span><br><span id="option6">kernel: <span class="optcolr">ok</span></span><br><span id="option7">sudo: <span class="optcolr">ok</span></span><br><span id="option8">disp: <span class="optcolr">' + display.status() + "</span></span><br>"; display.setColor("init");}, 600);
+		setTimeout(function() {
+			document.getElementById("rightBar").innerHTML = 'sys sts: ok<br>'; 
+		}, 200);
+		setTimeout(function() {
+			document.getElementById("rightBar").innerHTML = 'sys sts: ok<br>kernel: ok<br>';
+		}, 300);
+		setTimeout(function() {
+			document.getElementById("rightBar").innerHTML = 'sys sts: ok<br>kernel: ok<br>sudo: ok<br>';
+		}, 350);
+		setTimeout(function() {
+			document.getElementById("rightBar").innerHTML = '<span class="_j" id="right_title">System Overviewd</span><br><span id="option1">sys sts: <span class="optcolr">' + kernel_task() + '</span></span><br><span id="option2">kernel: <span class="optcolr">ok</span></span><br><span id="option3">sudo: <span class="optcolr">ok</span></span><br><span id="option4">disp: <span class="optcolr">' + display.status() + "</span></span><br><span id='option5'>enabl colors</span><br><span id='option6'>enabl debug mode</span><br><span id='option7'>enabl run num</span><br><span id='option8'>trgr kernel panic</span>"
+		}, 450);
+		setTimeout(function() {
+			document.getElementById("rightBar").innerHTML = '<span id="option1">toggle txt colors</span><br>';
+		}, 200);
+		setTimeout(function() {
+			document.getElementById("rightBar").innerHTML = '<span id="option1">toggle txt colors</span><br>enable debug mode<br>';
+		}, 300);
+		setTimeout(function() {
+			document.getElementById("rightBar").innerHTML = '<span id="option1">toggle txt colors</span><br>enable debug mode<br>enable run num<br>';
+		}, 350);
+		setTimeout(function() {
+			document.getElementById("rightBar").innerHTML = '<span id="option1">toggle txt colors</span><br>enable debug mode<br>enable run num<br>trgr kernel panic';
+		}, 400);
+		setTimeout(function() {
+			document.getElementById("rightBar").innerHTML = '<span id="option1">toggle txt colors</span><br>enable debug mode<br>enable run num<br>trgr kernel panic<br><span id="option5">sys sts: <span class="optcolr">ok</span></span>';
+		}, 500);
+		setTimeout(function() {
+			document.getElementById("rightBar").innerHTML = '<span id="option1">toggle txt colors</span><br>enable debug mode<br>enable run num<br>trgr kernel panic<br><span id="option5">sys sts: <span class="optcolr">ok</span></span><br>kernel: <span class="optcolr">ok</span>';
+		}, 560);
+		setTimeout(function() {
+			document.getElementById("rightBar").innerHTML = "<span id='option1'>toggle txt colors</span><br><span id='option2'>enable debug mode</span><br><span id='option3'>enable run num</span><br><span id='option4'>trgr kernel panic</span><br><span id='option5'>sys sts: <span class='optcolr'>" + kernel_task() + '</span></span><br><span id="option6">kernel: <span class="optcolr">ok</span></span><br><span id="option7">sudo: <span class="optcolr">ok</span></span><br><span id="option8">disp: <span class="optcolr">' + display.status() + "</span></span><br>"; display.setColor("init");
+		}, 600);
 		document.getElementById("rightBar").style.borderLeft = '1px solid #fff';
 	}
-	init_dp() { setTimeout(function() {consoleWrite("Welcome to <span class='_j'>Console V</span>.")},200); }
 	
-	deit_dp() { display.clear();}
+	// Initilize display
+	init_dp() {
+		setTimeout(function() {
+			consoleWrite("Welcome to <span class='_j'>Console V</span>.")
+		},200); 
+	}
+
+	// Deinitilize display
+	deit_dp() { 
+		display.clear();
+	}
+
+	// Deinitilize right bar
 	deit_rb() {
-		setTimeout(function() {document.getElementById("rightBar").innerHTML = '';}, 25);
-		setTimeout(function() {document.getElementById("rightBar").style.borderLeft = 'none';}, 100); }
+		setTimeout(function() {
+			document.getElementById("rightBar").innerHTML = '';
+		}, 25);
+		setTimeout(function() {
+			document.getElementById("rightBar").style.borderLeft = 'none';
+		}, 100); 
+	}
+	
+	// Deinitilize task bar
 	deit_tb() {
 		setTimeout(function() {document.getElementById("topBar").innerHTML = '<a id="time"></a>';}, 50);
 		setTimeout(function() {document.getElementById("topBar").style.borderBottom = 'none';},100);
